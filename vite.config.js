8f22892c33e4807abfea2f93cc7a947e2dc758cd
Germán Boquizo Sánchez <xekevara@gmail.com>
2023-12-21 16:31:29 +0100

    🎨 Format of the code

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: [
+        ...refreshPaths,
+        'app/Livewire/**',
+      ],
+    }),
+  ],
 });
